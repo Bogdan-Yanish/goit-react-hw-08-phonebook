978ce5364f49a49d9e942293332fee62aa59493c
BOGDAN YANISHEVSKYY <106109430+Bogdan-Yanish@users.noreply.github.com>
2023-02-04 14:55:28 +0200

    create phonebook to REDUX

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <>
+  <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
     <GlobalStyle />
-  </React.StrictMode>
+  </>
+    
+  
 );
